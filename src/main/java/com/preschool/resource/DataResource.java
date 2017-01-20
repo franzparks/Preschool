a212eb6ecd3629ccc57849a4e5f1dd6112564686
franzparks <franzparks@gmail.com>
2017-01-19 22:33:22 -0800

    add request mapper and make class a rest controller

--- src/main/java/com/preschool/resource/DataResource.java
+++ src/main/java/com/preschool/resource/DataResource.java
@@ -3,10 +3,16 @@
  */
 package com.preschool.resource;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author francis
  *
  */
+
+@RestController
+@RequestMapping("/api")
 public class DataResource {
 
 }
