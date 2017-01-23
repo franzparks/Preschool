01d7ba2faa0f009474d2ec7b48605060fd83ee70
franzparks <franzparks@gmail.com>
2017-01-23 12:47:41 -0800

    add constructor and feilds

--- src/main/java/com/preschool/domain/Result.java
+++ src/main/java/com/preschool/domain/Result.java
@@ -1,12 +1,22 @@
+
 /**
  * 
  */
 package com.preschool.domain;
+
+import java.util.List;
 
 /**
  * @author francis
  *
  */
 public class Result {
+	
+	private SearchInformation searchInformation;
+	private List<School> schools;
+	
+	public Result() {
+		
+	}
 
 }
