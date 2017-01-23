3060957d5ab1620ee51a71148cab7b3752f0196a
franzparks <franzparks@gmail.com>
2017-01-23 13:34:08 -0800

    clean up test case

--- src/test/java/com/preschool/CustomSearchTest.java
+++ src/test/java/com/preschool/CustomSearchTest.java
@@ -7,6 +7,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.preschool.search.CustomSearch;
 
