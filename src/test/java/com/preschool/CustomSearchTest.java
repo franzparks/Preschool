5c8dd1abdb5bd5345b0dc7e060a3e3a658cc3ce0
franzparks <franzparks@gmail.com>
2017-01-23 13:31:14 -0800

    add test class setup method

--- src/test/java/com/preschool/CustomSearchTest.java
+++ src/test/java/com/preschool/CustomSearchTest.java
@@ -3,6 +3,7 @@
  */
 package com.preschool;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -19,5 +20,10 @@ public class CustomSearchTest {
 	private final String cx = "014723624719242706501:ky6zn2teax4";
     private final String apiKey = "AIzaSyBFnKBQPESdi2sP1twKp59-3mBscTVw99k";
     CustomSearch $;
+    
+    @Before
+    public void setUp() throws Exception {
+        $ = new CustomSearch(cx, apiKey, 2);
+    }
 
 }
