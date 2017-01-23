ab9988cc0a7113d8f91a97c18c29d85ecdf46d0a
franzparks <franzparks@gmail.com>
2017-01-22 16:01:07 -0800

    add decorators and implement filter

--- src/main/java/com/preschool/config/RequestFilter.java
+++ src/main/java/com/preschool/config/RequestFilter.java
@@ -3,10 +3,18 @@
  */
 package com.preschool.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
 /**
  * @author francis
  *
  */
-public class RequestFilter {
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RequestFilter implements Filter{
 
 }
