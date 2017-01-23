aa30fc5c874820d9423aa0bcce67eb0590d725b1
franzparks <franzparks@gmail.com>
2017-01-23 13:29:50 -0800

    add test class fields

--- src/test/java/com/preschool/CustomSearchTest.java
+++ src/test/java/com/preschool/CustomSearchTest.java
@@ -3,10 +3,21 @@
  */
 package com.preschool;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.preschool.search.CustomSearch;
+
 /**
  * @author francis
  *
  */
+
+@RunWith(JUnit4.class)
 public class CustomSearchTest {
+	
+	private final String cx = "014723624719242706501:ky6zn2teax4";
+    private final String apiKey = "AIzaSyBFnKBQPESdi2sP1twKp59-3mBscTVw99k";
+    CustomSearch $;
 
 }
