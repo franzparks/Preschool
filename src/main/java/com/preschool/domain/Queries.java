f3f23d51af63be4a899151b191d34b316ae9e97a
franzparks <franzparks@gmail.com>
2017-01-23 11:52:07 -0800

    add getters and setters

--- src/main/java/com/preschool/domain/Queries.java
+++ src/main/java/com/preschool/domain/Queries.java
@@ -12,5 +12,13 @@ import java.util.List;
 public class Queries {
 	
 	private List<NextPage> nextPage;
+	
+	public List<NextPage> getNextPage() {
+		return nextPage;
+	}
+
+	public void setNextPage(List<NextPage> nextPage) {
+		this.nextPage = nextPage;
+	}
 
 }
