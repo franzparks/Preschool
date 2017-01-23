0613bd2e20e3d183d180def9cfffad1c9a9ad0d8
franzparks <franzparks@gmail.com>
2017-01-23 13:11:40 -0800

    add constructor fields

--- src/main/java/com/preschool/search/CustomSearch.java
+++ src/main/java/com/preschool/search/CustomSearch.java
@@ -8,5 +8,18 @@ package com.preschool.search;
  *
  */
 public class CustomSearch {
+	
+	private String cx;
+	private String apiKey;
+	private int num;
+	private int start = 1;
+	
+	public CustomSearch(String cx, String apiKey, int num) {
+		if (isNullOrEmpty(cx) || isNullOrEmpty(apiKey)) { throw new IllegalArgumentException("CX and API Key are required"); }
+		
+		setApiKey(apiKey);
+		setCx(cx);
+		setNum(num);
+	}
 
 }
