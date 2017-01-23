7ff3b2688a84c59a2ffdcd008700999f7d8730d7
franzparks <franzparks@gmail.com>
2017-01-22 18:10:38 -0800

    add some mock data

--- src/main/java/com/preschool/resource/DataResource.java
+++ src/main/java/com/preschool/resource/DataResource.java
@@ -24,7 +24,10 @@ public class DataResource {
 	@RequestMapping(value = "/schoolList", method = RequestMethod.GET)
     public List<School> bookList() {
 		ArrayList<School> list = new ArrayList<School>();
-		//list.add();
+		list.add(new School(1L,"Montesorri", "Welcome to the best Montesorri",
+				"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3U0wfMv-GMFQLjACk-nf_b1pdbpDHQkiviiAGyy8L0MZK2gUQeKjbSE3e"));
+		list.add(new School(2L,"Harker", "Welcome to Harker",
+				"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3U0wfMv-GMFQLjACk-nf_b1pdbpDHQkiviiAGyy8L0MZK2gUQeKjbSE3e"));
 		return list;
 	}
 
