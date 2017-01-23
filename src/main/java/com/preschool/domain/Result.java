2fbf5fecb3314cd1e68bef99b7c63b7e3c0cb5c5
franzparks <franzparks@gmail.com>
2017-01-23 12:49:09 -0800

    add getters and setters

--- src/main/java/com/preschool/domain/Result.java
+++ src/main/java/com/preschool/domain/Result.java
@@ -15,8 +15,36 @@ public class Result {
 	private SearchInformation searchInformation;
 	private List<School> schools;
 	
-	public Result() {
-		
+	public Result() {}
+
+	/**
+	 * @return the searchInformation
+	 */
+	public SearchInformation getSearchInformation() {
+		return searchInformation;
 	}
+
+	/**
+	 * @return the schools
+	 */
+	public List<School> getSchools() {
+		return schools;
+	}
+
+	/**
+	 * @param searchInformation the searchInformation to set
+	 */
+	public void setSearchInformation(SearchInformation searchInformation) {
+		this.searchInformation = searchInformation;
+	}
+
+	/**
+	 * @param schools the schools to set
+	 */
+	public void setSchools(List<School> schools) {
+		this.schools = schools;
+	}
+	
+	
 
 }
