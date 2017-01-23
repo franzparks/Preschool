df321a36a6f3a77850f4bdab06670c0555d283bd
franzparks <franzparks@gmail.com>
2017-01-23 11:55:24 -0800

    add getters and setters and helper methods

--- src/main/java/com/preschool/domain/MetaData.java
+++ src/main/java/com/preschool/domain/MetaData.java
@@ -15,6 +15,26 @@ public class MetaData {
 	public MetaData(String json) {
 		setJson(json);
 		setQueries();
+		
+		private String getJson() {
+			return json;
+		}
+		
+		private void setJson(String json) {
+			this.json = json;
+		}
+		
+		private JsonObject getJsonObject() {
+			return new JsonParser().parse(getJson()).getAsJsonObject();
+		}
+		
+		private void setQueries() {
+			queries = new Gson().fromJson(getJsonObject().get("queries"), Queries.class);
+		}
+		
+		public Queries getQueries() {
+			return queries;
+		}
 	}
 
 }
