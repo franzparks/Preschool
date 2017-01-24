59d26e8586290062db6145b71a4d01c2e20be775
franzparks <franzparks@gmail.com>
2017-01-24 13:31:58 -0800

    add config class

---
+++ src/main/java/com/preschool/config/GeneralConfig.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.preschool.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author francis
+ *
+ */
+@Configuration
+public class GeneralConfig {
+
+}
