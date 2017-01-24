671a34b57bbc5d60963ae5ccd3d4d279445275b8
franzparks <franzparks@gmail.com>
2017-01-24 13:33:58 -0800

    add config method

--- src/main/java/com/preschool/config/GeneralConfig.java
+++ src/main/java/com/preschool/config/GeneralConfig.java
@@ -5,11 +5,18 @@ package com.preschool.config;
 
 import org.springframework.context.annotation.Configuration;
 
+import com.preschool.search.CustomSearch;
+
 /**
  * @author francis
  *
  */
 @Configuration
 public class GeneralConfig {
+	
+	public CustomSearch makeCustomSearch(){
+		
+		return null;
+	}
 
 }
