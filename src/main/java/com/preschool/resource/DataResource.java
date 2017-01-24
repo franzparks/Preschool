8e5705533e0675456ee66beb2e9efa98877d69a3
franzparks <franzparks@gmail.com>
2017-01-24 13:47:37 -0800

    autowire a search component

--- src/main/java/com/preschool/resource/DataResource.java
+++ src/main/java/com/preschool/resource/DataResource.java
@@ -6,12 +6,14 @@ package com.preschool.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.preschool.domain.Result;
 import com.preschool.domain.School;
+import com.preschool.search.CustomSearch;
 import com.preschool.search.CustomSearchImp;
 
 /**
@@ -22,6 +24,9 @@ import com.preschool.search.CustomSearchImp;
 @RestController
 @RequestMapping("/api")
 public class DataResource {
+	
+	@Autowired
+	private CustomSearch search;
 	
 	@RequestMapping(value = "/schoolList", method = RequestMethod.GET)
     public List<School> bookList() {
