72219046eb4cb863038d3c0df3f23a7c7548cacb
franzparks <franzparks@gmail.com>
2017-01-24 10:31:18 -0800

    get items from google api in the service

--- src/main/java/com/preschool/resource/DataResource.java
+++ src/main/java/com/preschool/resource/DataResource.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.preschool.domain.Result;
 import com.preschool.domain.School;
+import com.preschool.search.CustomSearch;
 
 /**
  * @author francis
@@ -24,10 +26,13 @@ public class DataResource {
 	@RequestMapping(value = "/schoolList", method = RequestMethod.GET)
     public List<School> bookList() {
 		ArrayList<School> list = new ArrayList<School>();
-		//list.add(new School(1L,"Montesorri", "Welcome to the best Montesorri",
-		//		"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3U0wfMv-GMFQLjACk-nf_b1pdbpDHQkiviiAGyy8L0MZK2gUQeKjbSE3e"));
-		//list.add(new School(2L,"Harker", "Welcome to Harker",
-		//		"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ9sHLlSuNuhahdkDnIlA704LFzfcXP38_55jiTd2n3OecJrTRgnoZ3oOr7"));
+		
+		String cx = "014723624719242706501:ky6zn2teax4";
+	    String apiKey = "AIzaSyBFnKBQPESdi2sP1twKp59-3mBscTVw99k";
+	    CustomSearch $ = new CustomSearch(cx, apiKey, 10);
+	    
+		Result result = $.execute("Preschools");
+		list.addAll(result.getItems());
 		return list;
 	}
 
