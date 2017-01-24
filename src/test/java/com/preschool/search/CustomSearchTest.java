afe64b5ea4c6614f14523fce7454833e4444d453
franzparks <franzparks@gmail.com>
2017-01-24 10:16:28 -0800

    remove unused imports

--- src/test/java/com/preschool/search/CustomSearchTest.java
+++ src/test/java/com/preschool/search/CustomSearchTest.java
@@ -9,8 +9,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.preschool.domain.Result;
 import com.preschool.search.CustomSearch;
