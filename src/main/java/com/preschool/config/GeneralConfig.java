cb91193f05ae1d9fca0ee26d302af9939288ad00
franzparks <franzparks@gmail.com>
2017-01-24 13:46:39 -0800

    implement config method and add instance variables

--- src/main/java/com/preschool/config/GeneralConfig.java
+++ src/main/java/com/preschool/config/GeneralConfig.java
@@ -3,9 +3,14 @@
  */
 package com.preschool.config;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Configuration;
 
+import com.preschool.domain.Result;
+import com.preschool.domain.School;
 import com.preschool.search.CustomSearch;
+import com.preschool.search.CustomSearchImp;
 
 /**
  * @author francis
@@ -14,7 +19,17 @@ import com.preschool.search.CustomSearch;
 @Configuration
 public class GeneralConfig {
 	
+	private final String cx = "014723624719242706501:ky6zn2teax4";
+    private final String apiKey = "AIzaSyBFnKBQPESdi2sP1twKp59-3mBscTVw99k";
+	
 	public CustomSearch makeCustomSearch(){
+		
+			ArrayList<School> list = new ArrayList<School>();
+		
+		    CustomSearch $ = new CustomSearchImp(cx, apiKey, 10);
+		    
+			Result result = $.execute("Santa Clara Preschools");
+			list.addAll(result.getItems());
 		
 		return null;
 	}
