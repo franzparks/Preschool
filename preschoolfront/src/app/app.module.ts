f0a4ffe7628783554a54e9fad138d9ee13d2ac24
Francis Phiri <franzparks@gmail.com>
2017-05-04 15:25:14 +0800

    add shadow to nav

--- preschoolfront/src/app/app.module.ts
+++ preschoolfront/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { AppComponent } from './app.component';
     HttpModule,
     BrowserAnimationsModule,
     MdButtonModule, MdCheckboxModule,
-    MdToolbarModule.forRoot()
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
