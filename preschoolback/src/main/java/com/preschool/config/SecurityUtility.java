82f739b67690dd1dce6048211dadd621309ce573
Francis Phiri <franzparks@gmail.com>
2017-05-05 12:00:41 +0800

    add BCryptPasswordEncoder bean

--- preschoolback/src/main/java/com/preschool/config/SecurityUtility.java
+++ preschoolback/src/main/java/com/preschool/config/SecurityUtility.java
@@ -3,10 +3,20 @@
  */
 package com.preschool.config;
 
+import java.security.SecureRandom;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 /**
  * @author francisphiri
  *
  */
 public class SecurityUtility {
 	private static final String SALT = "salt"; //Salt should be protected carefully	
+	
+	@Bean
+	public static BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+	}
 }
