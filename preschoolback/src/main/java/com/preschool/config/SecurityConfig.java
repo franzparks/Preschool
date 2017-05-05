b138e4e72f70853e1213c8a7957e778d5f31b074
Francis Phiri <franzparks@gmail.com>
2017-05-05 12:17:17 +0800

    autowire env

--- preschoolback/src/main/java/com/preschool/config/SecurityConfig.java
+++ preschoolback/src/main/java/com/preschool/config/SecurityConfig.java
@@ -3,8 +3,11 @@
  */
 package com.preschool.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author francisphiri
@@ -14,4 +17,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private Environment env;
 }
