08e840d68d56cf708544f57bab8940dc8fc860c7
Francis Phiri <franzparks@gmail.com>
2017-05-05 09:39:29 +0800

    import md tabs module

--- preschoolfront/src/app/app.module.ts
+++ preschoolfront/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import {MdButtonModule, MdCheckboxModule,MdTabsModule} from '@angular/material';
 import { MdToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -20,6 +20,7 @@ import { AppComponent } from './app.component';
     HttpModule,
     BrowserAnimationsModule,
     MdButtonModule, MdCheckboxModule,
+    MdTabsModule
     
   ],
   providers: [],
