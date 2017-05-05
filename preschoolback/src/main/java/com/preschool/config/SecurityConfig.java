9134dd7cac38ffd6b09fc2f2eb4454f7458f5836
Francis Phiri <franzparks@gmail.com>
2017-05-05 12:19:47 +0800

    autowire passwordEncoder and add public matchers

--- preschoolback/src/main/java/com/preschool/config/SecurityConfig.java
+++ preschoolback/src/main/java/com/preschool/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author francisphiri
@@ -19,4 +20,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private Environment env;
+	
+	//@Autowired
+	//private UserSecurityService userSecurityService;
+	
+	private BCryptPasswordEncoder passwordEncoder() {
+		return SecurityUtility.passwordEncoder();
+	}
+
+	private static final String[] PUBLIC_MATCHERS = {
+			"/css/**",
+			"/js/**",
+			"/image/**",
+			"/school/**",
+			"/user/**"
+	};
 }
