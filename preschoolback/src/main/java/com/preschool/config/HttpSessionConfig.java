1eeedec867a49da1c558e75c75bea8a447a5200b
Francis Phiri <franzparks@gmail.com>
2017-05-05 14:02:17 +0800

    add redis dependencies and add a connection factory

--- preschoolback/src/main/java/com/preschool/config/HttpSessionConfig.java
+++ preschoolback/src/main/java/com/preschool/config/HttpSessionConfig.java
@@ -3,10 +3,18 @@
  */
 package com.preschool.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
 /**
  * @author francisphiri
  *
  */
+@EnableRedisHttpSession
 public class HttpSessionConfig {
-	
+	@Bean
+	public LettuceConnectionFactory connectionFactory() {
+		return new LettuceConnectionFactory();
+	}
 }
