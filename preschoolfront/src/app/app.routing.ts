3842822e461c5481f05bee5b996a98a2cc33504f
Francis Phiri <franzparks@gmail.com>
2017-05-08 20:38:14 +0800

    add initial routes

--- preschoolfront/src/app/app.routing.ts
+++ preschoolfront/src/app/app.routing.ts
@@ -2,3 +2,15 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 import {HomeComponent} from './components/home/home.component';
+
+const appRoutes: Routes = [
+	{
+		path: '',
+		redirectTo: '/home',
+		pathMatch: 'full'
+	},
+	{
+		path: 'home',
+		component: HomeComponent
+	}
+]
