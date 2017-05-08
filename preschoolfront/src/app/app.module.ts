b66a9fc78cf1b032eb05214f8ddba2391fadfba9
Francis Phiri <franzparks@gmail.com>
2017-05-08 20:36:22 +0800

    create nav-bar component

--- preschoolfront/src/app/app.module.ts
+++ preschoolfront/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { MdToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent
     
   ],
   imports: [
