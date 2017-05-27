fb2a1345c45ea1166ac8e1130a9000093a99d3ba
Francis Phiri <franzparks@gmail.com>
2017-05-26 23:55:12 -0700

    import school service

--- preschoolfront/src/app/components/home/home.component.ts
+++ preschoolfront/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { School } from '../models/school';
+import {SchoolService } from '../services/school.service';
 
 @Component({
   selector: 'app-home',
