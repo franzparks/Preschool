875a8a53ad986b23f8932383f32eadf845179a78
Francis Phiri <franzparks@gmail.com>
2017-05-27 00:00:04 -0700

    import dependencies

--- preschoolfront/src/app/services/school.service.ts
+++ preschoolfront/src/app/services/school.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import  {School} from '../models/school';
 
 @Injectable()
 export class SchoolService {
