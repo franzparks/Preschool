ccf2c47495e0d5bd6a295f5fe995df0b3ba4fe18
Francis Phiri <franzparks@gmail.com>
2017-05-27 00:37:21 -0700

    restore mock data

--- preschoolfront/src/app/services/mock-schools.ts
+++ preschoolfront/src/app/services/mock-schools.ts
@@ -3,9 +3,48 @@ export const SCHOOLS = [
 		id : 1,
 		title : 'The Harker School - Preschool',
 		subTitle : 'The bay\'s best',
-		summary :'',
+		summary :'At Harker’s preschool, our early learning program for 3-, 4- and young \
+		 5-year-olds emphasizes hands-on learning, intentional play and developmentally \
+		 appropriate classroom activities.\
+		 Beginnings matter. At Harker, we understand the importance of giving your child \
+		 the best possible foundation for an outstanding academic career. That starts with \
+		 choosing the right preschool to excite, nurture and challenge children so they are \
+		 not only ready for the next step but excited to get there.',
 		address : '4525 Union Ave, San Jose, CA 95124', 
 		phone : ' (408) 553-5700', 
 		rating : 4
-	}
+	},
+	{
+		id : 2,
+		title : 'Challenger',
+		subTitle: 'The  best of the bay',
+		summary :
+		'Challenger is the best preschool for your child in the SF bay area.\
+		      Our dedication to quality education is matched by none.',
+		address: '4525 Union Ave, San Jose, CA 95324', 
+		phone: ' (408) 553-5700', 
+		rating: 4
+	},
+	{
+		id: 3,
+		title : 'South Bay',
+		subTitle: 'The  best of the bay',
+		summary:
+		'South Bay is the best preschool for your child in the SF bay area.\
+		      Our dedication to quality education is matched by none.',
+		address: '4525 Union Ave, San Jose, CA 95324', 
+		phone: ' (408) 553-5700', 
+		rating: 3
+	},
+	{
+		id: 4,
+		title : 'Montessori',
+		subTitle: 'The  best of the bay',
+		summary:
+		'Montessori is the best preschool for your child in the SF bay area.\
+		      Our dedication to quality education is matched by none.',
+		address: '4525 Union Ave, San Jose, CA 95324', 
+		phone: ' (408) 553-5700', 
+		rating: 3
+	},
 ];
