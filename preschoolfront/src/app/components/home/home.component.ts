c74e5ca493b9e56ffeaa8194cff2a79a21757516
Francis Phiri <franzparks@gmail.com>
2017-05-26 23:54:27 -0700

    import school model

--- preschoolfront/src/app/components/home/home.component.ts
+++ preschoolfront/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { School } from '../models/school';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,7 +14,7 @@ export class HomeComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit() {
-		
+
 	}
 	  
 
