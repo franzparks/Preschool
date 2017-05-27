28f49160512f07dcc5ecaa118179aab66421dbed
Francis Phiri <franzparks@gmail.com>
2017-05-27 03:11:06 -0700

    import school service

--- preschoolfront/src/app/components/school-details/school-details.component.ts
+++ preschoolfront/src/app/components/school-details/school-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
 import {Params, ActivatedRoute, Router} from '@angular/router';
+
+import {SchoolService } from '../../services/school.service';
 
 @Component({
   selector: 'app-school-details',
