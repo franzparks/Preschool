c5b4c90c850631ccdd04d8ba5f91fbe176adce3d
Francis Phiri <franzparks@gmail.com>
2017-05-26 23:45:14 -0700

    add sample data in mock service

--- preschoolfront/src/app/services/mock-schools.ts
+++ preschoolfront/src/app/services/mock-schools.ts
@@ -1,7 +1,19 @@
 export const SCHOOLS = [
 	{
 		id: 1,
-		name : 'Harker'
+		title : 'The Harker School - Preschool',
+		subTitle: 'The bay\'s best',
+		summary:
+		'At Harker\'s preschool, our early learning program for 3-, 4- and young 5-year-olds emphasizes\
+		hands-on learning,\
+		 intentional play and developmentally appropriate classroom activities.\
+		 Beginnings matter. At Harker, we understand the importance of giving your\ 
+		 child the best possible foundation for an outstanding academic career. That starts with choosing the right\
+		  preschool to excite, nurture and challenge children so they are not only ready for \
+		  the next step but excited to get there.',
+		address: '4525 Union Ave, San Jose, CA 95124', 
+		phone: ' (408) 553-5700', 
+		rating: 3
 	},
 	{
 		id: 2,
