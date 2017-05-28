c9f5de6b2b9b70546f4d4ed1434e5e025c848675
Francis Phiri <franzparks@gmail.com>
2017-05-27 19:34:03 -0700

    import dependencies

--- preschoolfront/src/app/services/login.service.ts
+++ preschoolfront/src/app/services/login.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {AppConst} from '../constants/app-const';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class LoginService {
