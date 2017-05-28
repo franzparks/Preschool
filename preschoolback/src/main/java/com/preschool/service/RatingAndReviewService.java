e499ea3ad6e092402b541e9e34f5d7c83fd57102
Francis Phiri <franzparks@gmail.com>
2017-05-28 00:11:25 -0700

    create methods to implement

--- preschoolback/src/main/java/com/preschool/service/RatingAndReviewService.java
+++ preschoolback/src/main/java/com/preschool/service/RatingAndReviewService.java
@@ -3,10 +3,26 @@
  */
 package com.preschool.service;
 
+import java.util.List;
+
+import com.preschool.domain.RatingAndReview;
+import com.preschool.domain.School;
+
 /**
  * @author francisphiri
  *
  */
 public interface RatingAndReviewService {
 	
+	List<RatingAndReview> findAll();
+	
+	RatingAndReview findOne(Long id);
+	
+	RatingAndReview save(RatingAndReview book);
+	
+	List<RatingAndReview> blurrySearch(String title);
+	
+	RatingAndReview updateRatingAndReview(RatingAndReview ratingAndReview);
+	
+	void removeOne(Long id);
 }
