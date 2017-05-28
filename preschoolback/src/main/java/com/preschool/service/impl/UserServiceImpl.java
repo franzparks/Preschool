5df9442597ae56a277aa591174e218dab0ada46f
Francis Phiri <franzparks@gmail.com>
2017-05-28 12:26:38 -0700

    add annotation and implement UserService

--- preschoolback/src/main/java/com/preschool/service/impl/UserServiceImpl.java
+++ preschoolback/src/main/java/com/preschool/service/impl/UserServiceImpl.java
@@ -3,10 +3,49 @@
  */
 package com.preschool.service.impl;
 
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.preschool.domain.User;
+import com.preschool.domain.security.UserRole;
+import com.preschool.service.UserService;
+
 /**
  * @author francisphiri
  *
  */
-public class UserServiceImpl {
+@Service
+public class UserServiceImpl implements UserService{
+
+	@Override
+    public User createUser(User user, Set<UserRole> userRoles) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public User findByUsername(String username) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public User findByEmail(String email) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public User save(User user) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public User findById(Long id) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
 	
 }
