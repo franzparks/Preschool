583848fb7e661123545688569a27ac49b8090c8f
Francis Phiri <franzparks@gmail.com>
2017-05-28 16:38:10 -0700

    add newUser endpoint

--- preschoolback/src/main/java/com/preschool/resource/UserResource.java
+++ preschoolback/src/main/java/com/preschool/resource/UserResource.java
@@ -3,11 +3,26 @@
  */
 package com.preschool.resource;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.preschool.config.SecurityUtility;
+import com.preschool.domain.User;
+import com.preschool.domain.security.Role;
+import com.preschool.domain.security.UserRole;
 import com.preschool.service.UserService;
 import com.preschool.utility.MailConstructor;
 
@@ -27,5 +42,42 @@ public class UserResource {
 
 	@Autowired
 	private JavaMailSender mailSender;
+	
+	@RequestMapping(value = "/newUser", method = RequestMethod.POST)
+	public ResponseEntity newUserPost(HttpServletRequest request, @RequestBody HashMap<String, String> mapper)
+			throws Exception {
+		String username = mapper.get("username");
+		String userEmail = mapper.get("email");
+
+		if (userService.findByUsername(username) != null) {
+			return new ResponseEntity("usernameExists", HttpStatus.BAD_REQUEST);
+		}
+
+		if (userService.findByEmail(userEmail) != null) {
+			return new ResponseEntity("emailExists", HttpStatus.BAD_REQUEST);
+		}
+
+		User user = new User();
+		user.setUsername(username);
+		user.setEmail(userEmail);
+
+		String password = SecurityUtility.randomPassword();
+
+		String encryptedPassword = SecurityUtility.passwordEncoder().encode(password);
+		user.setPassword(encryptedPassword);
+
+		Role role = new Role();
+		role.setRoleId(1);
+		role.setName("ROLE_USER");
+		Set<UserRole> userRoles = new HashSet<>();
+		userRoles.add(new UserRole(user, role));
+		userService.createUser(user, userRoles);
+
+		SimpleMailMessage email = mailConstructor.constructNewUserEmail(user, password);
+		mailSender.send(email);
+
+		return new ResponseEntity("User Added Successfully!", HttpStatus.OK);
+
+	}
 	
 }
