d7f1950aa5e2fea58bc592828ac4af6027ba5eb6
Francis Phiri <franzparks@gmail.com>
2017-05-27 23:34:17 -0700

    add UserRepository

--- preschoolback/src/main/java/com/preschool/config/repository/UserRepository.java
+++ preschoolback/src/main/java/com/preschool/config/repository/UserRepository.java
@@ -3,10 +3,18 @@
  */
 package com.preschool.config.repository;
 
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.preschool.domain.User;
+
 /**
  * @author francisphiri
  *
  */
-public interface UserRepository {
-	
+public interface UserRepository extends CrudRepository<User, Long>{
+	User findByUsername(String username);
+	User findByEmail(String email);
+	List<User> findAll();
 }
