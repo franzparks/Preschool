6ec8c08f97189ca7388905f1c282066ccd5fb013
Francis Phiri <franzparks@gmail.com>
2017-05-27 22:25:41 -0700

    add more fields

--- preschoolback/src/main/java/com/preschool/domain/User.java
+++ preschoolback/src/main/java/com/preschool/domain/User.java
@@ -4,10 +4,21 @@
 package com.preschool.domain;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * @author francisphiri
@@ -15,5 +26,25 @@ import org.springframework.security.core.userdetails.UserDetails;
  */
 @Entity
 public class User implements UserDetails, Serializable{
+	
+private static final long serialVersionUID = 902783495L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="Id", nullable=false, updatable = false)
+	private Long id;
+	
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+	private String email;
+	private String phone;
+	private boolean enabled = true;
+	
+	@OneToMany(mappedBy = "user", cascade=CascadeType.ALL, fetch = FetchType.EAGER)
+	@JsonIgnore
+	private Set<UserRole> userRoles = new HashSet<>();
 	
 }
