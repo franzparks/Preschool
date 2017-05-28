80faf3d356b1a08393c24bb1567306c096e856b6
Francis Phiri <franzparks@gmail.com>
2017-05-27 22:22:02 -0700

    add getters and setters

--- preschoolback/src/main/java/com/preschool/domain/School.java
+++ preschoolback/src/main/java/com/preschool/domain/School.java
@@ -52,6 +52,88 @@ public class School implements Serializable{
 	@Transient
 	private MultipartFile schoolImage;
 
+	public Long getId() {
+		return id;
+	}
+
+	public List<RatingAndReview> getRatingAndReviewsList() {
+		return ratingAndReviewsList;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getSubTitle() {
+		return subTitle;
+	}
+
+	public String getSnippet() {
+		return snippet;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public int getAverageRating() {
+		return averageRating;
+	}
+
+	public MultipartFile getSchoolImage() {
+		return schoolImage;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setRatingAndReviewsList(List<RatingAndReview> ratingAndReviewsList) {
+		this.ratingAndReviewsList = ratingAndReviewsList;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setSubTitle(String subTitle) {
+		this.subTitle = subTitle;
+	}
+
+	public void setSnippet(String snippet) {
+		this.snippet = snippet;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public void setAverageRating(int averageRating) {
+		this.averageRating = averageRating;
+	}
+
+	public void setSchoolImage(MultipartFile schoolImage) {
+		this.schoolImage = schoolImage;
+	}
+	
+	
+
 	
 	
 
