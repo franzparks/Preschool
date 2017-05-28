df058646fc596e2f055e231c9ede62ed09ea35e4
Francis Phiri <franzparks@gmail.com>
2017-05-27 23:52:48 -0700

    create methods to implement

--- preschoolback/src/main/java/com/preschool/service/SchoolService.java
+++ preschoolback/src/main/java/com/preschool/service/SchoolService.java
@@ -3,10 +3,22 @@
  */
 package com.preschool.service;
 
+import java.util.List;
+
+import com.preschool.domain.School;
+
 /**
  * @author francisphiri
  *
  */
 public interface SchoolService {
+	List<School> findAll();
 	
+	School findOne(Long id);
+	
+	School save(School book);
+	
+	List<School> blurrySearch(String title);
+	
+	void removeOne(Long id);
 }
