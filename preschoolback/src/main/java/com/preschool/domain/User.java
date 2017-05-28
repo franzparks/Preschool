a3de13bcaaaa357521fc52ba6912cbb573bfa975
Francis Phiri <franzparks@gmail.com>
2017-05-27 22:38:38 -0700

    add missing fields

--- preschoolback/src/main/java/com/preschool/domain/User.java
+++ preschoolback/src/main/java/com/preschool/domain/User.java
@@ -4,6 +4,7 @@
 package com.preschool.domain;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,6 +18,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -51,6 +53,112 @@ private static final long serialVersionUID = 902783495L;
 	
 	@OneToMany(mappedBy="user")
 	private List<RatingAndReview> reviewsList;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public List<RatingAndReview> getReviewsList() {
+		return reviewsList;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+
+	public void setReviewsList(List<RatingAndReview> reviewsList) {
+		this.reviewsList = reviewsList;
+	}
+
+	@Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public boolean isAccountNonExpired() {
+	    // TODO Auto-generated method stub
+	    return false;
+    }
+
+	@Override
+    public boolean isAccountNonLocked() {
+	    // TODO Auto-generated method stub
+	    return false;
+    }
+
+	@Override
+    public boolean isCredentialsNonExpired() {
+	    // TODO Auto-generated method stub
+	    return false;
+    }
+	
+	
 	
 	
 	
