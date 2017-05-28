964033578a499dcb1782507aabef6624cc6c6553
Francis Phiri <franzparks@gmail.com>
2017-05-27 22:24:05 -0700

    add annotations

--- preschoolback/src/main/java/com/preschool/domain/User.java
+++ preschoolback/src/main/java/com/preschool/domain/User.java
@@ -5,12 +5,15 @@ package com.preschool.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+
 import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  * @author francisphiri
  *
  */
+@Entity
 public class User implements UserDetails, Serializable{
 	
 }
