258e543345c6c047a8b8eaad0b65f7ca7946da6a
Francis Phiri <franzparks@gmail.com>
2017-05-27 21:12:41 -0700

    clean up

--- preschoolback/src/main/java/com/preschool/domain/School.java
+++ preschoolback/src/main/java/com/preschool/domain/School.java
@@ -9,12 +9,6 @@ package com.preschool.domain;
  */
 public class School {
 	
-	/**
-	 * @param title
-	 * @param link
-	 * @param snippet
-	 * @param formattedUrl
-	 */
 	
 	private String title;
 	private String link;
@@ -22,48 +16,10 @@ public class School {
 	private String formattedUrl;
 	
 	
-	/*public School(String title, String link, String snippet, String formattedUrl) {
-		super();
-		this.title = title;
-		this.link = link;
-		this.snippet = snippet;
-		this.formattedUrl = formattedUrl;
-	}*/
-	
 	
 	public School(){}
 
-	public String getTitle() {
-		return title;
-	}
-
-	public String getLink() {
-		return link;
-	}
-
-	public String getSnippet() {
-		return snippet;
-	}
-
-	public String getFormattedUrl() {
-		return formattedUrl;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public void setLink(String link) {
-		this.link = link;
-	}
-
-	public void setSnippet(String snippet) {
-		this.snippet = snippet;
-	}
-
-	public void setFormattedUrl(String formattedUrl) {
-		this.formattedUrl = formattedUrl;
-	}
+	
 	
 
 }
