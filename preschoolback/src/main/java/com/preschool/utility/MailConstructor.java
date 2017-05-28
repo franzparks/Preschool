b948129e090ca61db47f91f3bdd5701f1a39fa59
Francis Phiri <franzparks@gmail.com>
2017-05-28 14:15:44 -0700

    implement constructNewUserEmail method

--- preschoolback/src/main/java/com/preschool/utility/MailConstructor.java
+++ preschoolback/src/main/java/com/preschool/utility/MailConstructor.java
@@ -5,7 +5,9 @@ package com.preschool.utility;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
 
 /**
  * @author francisphiri
@@ -19,4 +21,16 @@ public class MailConstructor {
 	
 	@Autowired
 	private TemplateEngine templateEngine;
+	
+	public SimpleMailMessage constructNewUserEmail(User user, String password) {
+		String message="\nPlease use the following credentials to log in and edit your personal information including your own password."
+				+ "\nUsername:"+user.getUsername()+"\nPassword:"+password;
+		
+		SimpleMailMessage email = new SimpleMailMessage();
+		email.setTo(user.getEmail());
+		email.setSubject("Le's Bookstore - New User");
+		email.setText(message);
+		email.setFrom(env.getProperty("support.email"));
+		return email;
+	}
 }
