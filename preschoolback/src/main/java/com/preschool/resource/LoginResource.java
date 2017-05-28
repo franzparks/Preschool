ba38c5e96deafc31fe2f8aa82c91b51af33da245
Francis Phiri <franzparks@gmail.com>
2017-05-28 16:31:24 -0700

    add logout method

--- preschoolback/src/main/java/com/preschool/resource/LoginResource.java
+++ preschoolback/src/main/java/com/preschool/resource/LoginResource.java
@@ -12,7 +12,9 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.preschool.service.UserService;
@@ -45,4 +47,9 @@ public class LoginResource {
 		return new ResponseEntity("Session Active!", HttpStatus.OK);
 	}
 	
+	@RequestMapping(value="/user/logout", method=RequestMethod.POST)
+	public ResponseEntity logout(){
+		SecurityContextHolder.clearContext();
+		return new ResponseEntity("Logged out Successfully!", HttpStatus.OK);
+	}
 }
