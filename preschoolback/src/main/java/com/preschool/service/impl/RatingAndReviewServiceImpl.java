7055fe3c32a574d55e0997b2a49ad30c8bd708a6
Francis Phiri <franzparks@gmail.com>
2017-05-28 12:38:50 -0700

    create RatingAndReviewServiceImpl

---
+++ preschoolback/src/main/java/com/preschool/service/impl/RatingAndReviewServiceImpl.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.preschool.service.impl;
+
+import java.util.List;
+
+import com.preschool.domain.RatingAndReview;
+import com.preschool.service.RatingAndReviewService;
+
+/**
+ * @author francisphiri
+ *
+ */
+public class RatingAndReviewServiceImpl implements RatingAndReviewService {
+	
+	/* (non-Javadoc)
+	 * @see com.preschool.service.RatingAndReviewService#findAll()
+	 */
+	@Override
+	public List<RatingAndReview> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.preschool.service.RatingAndReviewService#findOne(java.lang.Long)
+	 */
+	@Override
+	public RatingAndReview findOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.preschool.service.RatingAndReviewService#save(com.preschool.domain.RatingAndReview)
+	 */
+	@Override
+	public RatingAndReview save(RatingAndReview book) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.preschool.service.RatingAndReviewService#blurrySearch(java.lang.String)
+	 */
+	@Override
+	public List<RatingAndReview> blurrySearch(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.preschool.service.RatingAndReviewService#updateRatingAndReview(com.preschool.domain.RatingAndReview)
+	 */
+	@Override
+	public RatingAndReview updateRatingAndReview(RatingAndReview ratingAndReview) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.preschool.service.RatingAndReviewService#removeOne(java.lang.Long)
+	 */
+	@Override
+	public void removeOne(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
