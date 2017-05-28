8aa430a03d9c0dba163f620c658428f6c4750938
Francis Phiri <franzparks@gmail.com>
2017-05-28 16:34:59 -0700

    autowire services

--- preschoolback/src/main/java/com/preschool/resource/UserResource.java
+++ preschoolback/src/main/java/com/preschool/resource/UserResource.java
@@ -3,8 +3,13 @@
  */
 package com.preschool.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.preschool.service.UserService;
+import com.preschool.utility.MailConstructor;
 
 /**
  * @author francisphiri
@@ -13,5 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserResource {
+	
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private MailConstructor mailConstructor;
+
+	@Autowired
+	private JavaMailSender mailSender;
 	
 }
