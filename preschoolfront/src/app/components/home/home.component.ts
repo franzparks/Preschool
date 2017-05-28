d359468987382fb02a2b749371c12788e21fc943
Francis Phiri <franzparks@gmail.com>
2017-05-27 17:23:16 -0700

    import http

--- preschoolfront/src/app/components/home/home.component.ts
+++ preschoolfront/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Http } from '@angular/http';
 import {Params, ActivatedRoute, Router} from '@angular/router';
 
 import { School } from '../../models/school';
