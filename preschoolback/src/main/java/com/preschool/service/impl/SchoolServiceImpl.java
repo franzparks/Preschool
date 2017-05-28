21b51e31db8107a23421e616ef7377d00ca32f0a
Francis Phiri <franzparks@gmail.com>
2017-05-28 01:51:40 -0700

    implement SchoolService

--- preschoolback/src/main/java/com/preschool/service/impl/SchoolServiceImpl.java
+++ preschoolback/src/main/java/com/preschool/service/impl/SchoolServiceImpl.java
@@ -3,10 +3,12 @@
  */
 package com.preschool.service.impl;
 
+import com.preschool.service.SchoolService;
+
 /**
  * @author francisphiri
  *
  */
-public class SchoolServiceImpl {
+public class SchoolServiceImpl implements SchoolService{
 	
 }
