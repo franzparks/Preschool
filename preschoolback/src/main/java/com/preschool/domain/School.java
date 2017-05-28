a2c6fea43887f316d9382af858fa8d6b2919e24d
Francis Phiri <franzparks@gmail.com>
2017-05-27 21:44:44 -0700

    add annotations

--- preschoolback/src/main/java/com/preschool/domain/School.java
+++ preschoolback/src/main/java/com/preschool/domain/School.java
@@ -6,6 +6,9 @@ package com.preschool.domain;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Transient;
 
 import org.springframework.web.multipart.MultipartFile;
@@ -20,7 +23,10 @@ public class School implements Serializable{
 	
 	private static final long serialVersionUID=425345L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+	
 	private String title;
 	private String subTitle;
 	private String snippet;
