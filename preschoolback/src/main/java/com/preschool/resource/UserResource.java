592b09b54abfabdcd3371cdb6fd27c9c32f77364
Francis Phiri <franzparks@gmail.com>
2017-05-28 16:34:02 -0700

    add annotations and user endpoint

--- preschoolback/src/main/java/com/preschool/resource/UserResource.java
+++ preschoolback/src/main/java/com/preschool/resource/UserResource.java
@@ -3,10 +3,15 @@
  */
 package com.preschool.resource;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author francisphiri
  *
  */
+@RestController
+@RequestMapping("/user")
 public class UserResource {
 	
 }
