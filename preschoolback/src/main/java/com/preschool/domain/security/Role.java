9f243df8accfc14c9c7fdb13522f4e308f9627b2
Francis Phiri <franzparks@gmail.com>
2017-05-27 22:42:15 -0700

    add annotations

--- preschoolback/src/main/java/com/preschool/domain/security/Role.java
+++ preschoolback/src/main/java/com/preschool/domain/security/Role.java
@@ -5,10 +5,13 @@ package com.preschool.domain.security;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+
 /**
  * @author francisphiri
  *
  */
+@Entity
 public class Role implements Serializable{
 	
 }
