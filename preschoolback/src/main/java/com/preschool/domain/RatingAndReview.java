0502f6bd8cf48946a720420c728dfb562d3dff6a
Francis Phiri <franzparks@gmail.com>
2017-05-27 22:13:05 -0700

    add id and serial fields

---
+++ preschoolback/src/main/java/com/preschool/domain/RatingAndReview.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.preschool.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author francisphiri
+ *
+ */
+public class RatingAndReview {
+	
+private static final long serialVersionUID=425345L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	
+	
+}
