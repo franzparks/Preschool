64e07634b50683c2f86e94e8ed07fbd7d932b876
Francis Phiri <franzparks@gmail.com>
2017-05-28 00:02:21 -0700

    change to class

--- preschoolback/src/main/java/com/preschool/service/UserSecurityService.java
+++ preschoolback/src/main/java/com/preschool/service/UserSecurityService.java
@@ -5,15 +5,21 @@ package com.preschool.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.preschool.config.repository.UserRepository;
 
 /**
  * @author francisphiri
  *
  */
 @Service
-public interface UserSecurityService implements UserDetailsService{
+public class UserSecurityService implements UserDetailsService{
 	
 	private static final Logger LOG = LoggerFactory.getLogger(UserSecurityService.class);
+	
+	@Autowired 
+	private UserRepository userRepository;
 	
 }
