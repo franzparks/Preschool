405ed1f7226588d44567757248b0f88e3b33f58e
Francis Phiri <franzparks@gmail.com>
2017-05-28 16:55:31 -0700

    add addSchool endpoint

--- preschoolback/src/main/java/com/preschool/resource/SchoolResource.java
+++ preschoolback/src/main/java/com/preschool/resource/SchoolResource.java
@@ -4,9 +4,12 @@
 package com.preschool.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.preschool.domain.School;
 import com.preschool.service.SchoolService;
 
 /**
@@ -19,5 +22,10 @@ public class SchoolResource {
 	
 	@Autowired
 	private SchoolService schoolService;
+	
+	@RequestMapping (value="/addSchool", method=RequestMethod.POST)
+	public School addSchoolPost(@RequestBody School school) {
+		return schoolService.save(school);
+	}
 	
 }
