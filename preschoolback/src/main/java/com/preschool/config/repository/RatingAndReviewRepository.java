85c4f04a5d2bf7ea61b67bc231a4f95f9c9c7570
Francis Phiri <franzparks@gmail.com>
2017-05-27 23:43:06 -0700

    add search queries

--- preschoolback/src/main/java/com/preschool/config/repository/RatingAndReviewRepository.java
+++ preschoolback/src/main/java/com/preschool/config/repository/RatingAndReviewRepository.java
@@ -3,9 +3,12 @@
  */
 package com.preschool.config.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.preschool.domain.RatingAndReview;
+import com.preschool.domain.School;
 import com.preschool.domain.User;
 
 /**
@@ -14,4 +17,7 @@ import com.preschool.domain.User;
  */
 public interface RatingAndReviewRepository extends CrudRepository<RatingAndReview, Long>{
 	
+	List<RatingAndReview> findByUser(User user);
+	List<RatingAndReview> findBySchool(School school);
+	List<RatingAndReview> findByMessageContaining(String message);
 }
