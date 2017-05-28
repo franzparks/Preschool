c1b0a1cece02163de5577456325e772d85a85eb1
Francis Phiri <franzparks@gmail.com>
2017-05-28 01:52:02 -0700

    implement SchoolService methods

--- preschoolback/src/main/java/com/preschool/service/impl/SchoolServiceImpl.java
+++ preschoolback/src/main/java/com/preschool/service/impl/SchoolServiceImpl.java
@@ -3,6 +3,9 @@
  */
 package com.preschool.service.impl;
 
+import java.util.List;
+
+import com.preschool.domain.School;
 import com.preschool.service.SchoolService;
 
 /**
@@ -10,5 +13,35 @@ import com.preschool.service.SchoolService;
  *
  */
 public class SchoolServiceImpl implements SchoolService{
+
+	@Override
+    public List<School> findAll() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public School findOne(Long id) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public School save(School book) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public List<School> blurrySearch(String title) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
+    public void removeOne(Long id) {
+	    // TODO Auto-generated method stub
+	    
+    }
 	
 }
