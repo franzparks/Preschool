6a0dd1351f58c02215b08dbe557adc01c5c93ba3
Francis Phiri <franzparks@gmail.com>
2017-05-29 15:27:23 -0700

    import required dependencies

--- preschoolfront/src/app/services/user.service.ts
+++ preschoolfront/src/app/services/user.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {AppConst} from '../constants/app-const';
+import {User} from '../models/user';
 
 @Injectable()
 export class UserService {
