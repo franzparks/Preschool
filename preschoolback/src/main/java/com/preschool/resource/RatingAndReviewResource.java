7e6e55ad97e91ebd066f4266bcec3fdb7908e248
Francis Phiri <franzparks@gmail.com>
2017-05-28 19:22:16 -0700

    update endpoint and add user service

--- preschoolback/src/main/java/com/preschool/resource/RatingAndReviewResource.java
+++ preschoolback/src/main/java/com/preschool/resource/RatingAndReviewResource.java
@@ -3,8 +3,12 @@
  */
 package com.preschool.resource;
 
+import java.security.Principal;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +20,7 @@ import com.preschool.domain.RatingAndReview;
 import com.preschool.domain.School;
 import com.preschool.domain.User;
 import com.preschool.service.RatingAndReviewService;
+import com.preschool.service.UserService;
 
 /**
  * @author francisphiri
@@ -25,7 +30,11 @@ import com.preschool.service.RatingAndReviewService;
 @RequestMapping("/review")
 public class RatingAndReviewResource {
 	
+	@Autowired
 	private RatingAndReviewService ratingAndReviewService;
+	
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping (value="/all", method=RequestMethod.POST)
 	public List<RatingAndReview> findAllBySchool(@RequestBody School school){
@@ -43,10 +52,17 @@ public class RatingAndReviewResource {
 		return ratingAndReview;	
 	}
 	
-	@RequestMapping (value="/save", method=RequestMethod.POST)
-	RatingAndReview save(@RequestBody RatingAndReview ratingAndReview){
-		RatingAndReview ratingAndReviewOutPut = ratingAndReviewService.save(ratingAndReview);
-		return 	ratingAndReviewOutPut;
+
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public ResponseEntity addNewReviewPost(
+			@RequestBody RatingAndReview ratingAndReview, Principal principal) {
+		User user = userService.findByUsername(principal.getName());
+		
+		ratingAndReview.setUser(user);
+		
+		ratingAndReviewService.save(ratingAndReview);
+		
+		return new ResponseEntity("Review Added Successfully!", HttpStatus.OK);
 	}
 	
 	
