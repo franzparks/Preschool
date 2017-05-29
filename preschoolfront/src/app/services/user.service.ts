1f42f60fc92db069bdbe7ddf16242f147239bfd5
Francis Phiri <franzparks@gmail.com>
2017-05-29 15:32:12 -0700

    add newUser handler

--- preschoolfront/src/app/services/user.service.ts
+++ preschoolfront/src/app/services/user.service.ts
@@ -5,11 +5,25 @@ import {User} from '../models/user';
 
 @Injectable()
 export class UserService {
-	
+
 	private serverPath: string = AppConst.serverPath;
 
   	constructor(
   		private http:Http
   	) { }
+
+  	newUser(username: string, email:string) {
+  	let url = this.serverPath+'/user/newUser';
+  	let userInfo = {
+  		"username" : username,
+  		"email" : email
+  	}
+  	let tokenHeader = new Headers({
+  		'Content-Type' : 'application/json',
+  		'x-auth-token' : localStorage.getItem('xAuthToken')
+  	});
+
+  	return this.http.post(url, JSON.stringify(userInfo), {headers : tokenHeader});
+  }
 
 }
