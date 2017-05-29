f20bd29dfa9c374e0b1735d8998022b284fc6b7a
Francis Phiri <franzparks@gmail.com>
2017-05-28 17:08:44 -0700

    add addSchool/image endpoint

--- preschoolback/src/main/java/com/preschool/resource/SchoolResource.java
+++ preschoolback/src/main/java/com/preschool/resource/SchoolResource.java
@@ -3,11 +3,27 @@
  */
 package com.preschool.resource;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.preschool.domain.School;
 import com.preschool.service.SchoolService;
@@ -26,6 +42,30 @@ public class SchoolResource {
 	@RequestMapping (value="/addSchool", method=RequestMethod.POST)
 	public School addSchoolPost(@RequestBody School school) {
 		return schoolService.save(school);
+	}
+	
+	@RequestMapping(value="/addSchool/image", method=RequestMethod.POST)
+	public ResponseEntity upload(
+			@RequestParam("id") Long id,
+			HttpServletResponse response, HttpServletRequest request){
+		try {
+			School school = schoolService.findOne(id);
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			Iterator<String> it = multipartRequest.getFileNames();
+			MultipartFile multipartFile = multipartRequest.getFile(it.next());
+			String fileName = id+".png";
+			
+			
+			byte[] bytes = multipartFile.getBytes();
+			BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("src/main/resources/static/image/school/"+fileName)));
+			stream.write(bytes);
+			stream.close();
+			
+			return new ResponseEntity("Upload Success!", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity("Upload failed!", HttpStatus.BAD_REQUEST);
+		}
 	}
 	
 }
