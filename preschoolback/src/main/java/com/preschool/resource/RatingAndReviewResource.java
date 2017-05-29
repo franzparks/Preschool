e3f8527e0c40b5cb2f4d9e92ccfb23ae7bbdb8ab
Francis Phiri <franzparks@gmail.com>
2017-05-28 17:30:42 -0700

    add rest controller annotation

--- preschoolback/src/main/java/com/preschool/resource/RatingAndReviewResource.java
+++ preschoolback/src/main/java/com/preschool/resource/RatingAndReviewResource.java
@@ -3,10 +3,13 @@
  */
 package com.preschool.resource;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author francisphiri
  *
  */
+@RestController
 public class RatingAndReviewResource {
 	
 }
