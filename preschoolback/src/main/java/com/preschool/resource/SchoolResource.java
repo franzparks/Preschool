aa87fc58ff0976cd9c7dc77768f74afddd8112e7
Francis Phiri <franzparks@gmail.com>
2017-05-28 17:20:17 -0700

    add school/{id} endpoint

--- preschoolback/src/main/java/com/preschool/resource/SchoolResource.java
+++ preschoolback/src/main/java/com/preschool/resource/SchoolResource.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -118,6 +119,11 @@ public class SchoolResource {
 		return new ResponseEntity("Remove Success!", HttpStatus.OK);
 	}
 	
+	@RequestMapping("/{id}")
+	public School getBook(@PathVariable("id") Long id){
+		School book = schoolService.findOne(id);
+		return book;
+	}
 	
 	
 }
