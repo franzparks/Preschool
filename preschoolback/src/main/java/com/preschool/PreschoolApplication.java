0b4350c3741f16bf950576ac2389326a23d2df07
Francis Phiri <franzparks@gmail.com>
2017-05-28 19:59:21 -0700

    autowire UserService

--- preschoolback/src/main/java/com/preschool/PreschoolApplication.java
+++ preschoolback/src/main/java/com/preschool/PreschoolApplication.java
@@ -3,13 +3,19 @@ package com.preschool;
  * @author francis
  *
  */
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.preschool.service.UserService;
+
 @SpringBootApplication
 public class PreschoolApplication implements CommandLineRunner{
 
+	@Autowired
+	private UserService userService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PreschoolApplication.class, args);
 	}
