ee9a70dbc582581b5bc761c898ef4ed70415c243
Francis Phiri <franzparks@gmail.com>
2017-05-28 19:58:41 -0700

    implement CommandLineRunner

--- preschoolback/src/main/java/com/preschool/PreschoolApplication.java
+++ preschoolback/src/main/java/com/preschool/PreschoolApplication.java
@@ -3,13 +3,20 @@ package com.preschool;
  * @author francis
  *
  */
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PreschoolApplication {
+public class PreschoolApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PreschoolApplication.class, args);
 	}
+
+	@Override
+    public void run(String... arg0) throws Exception {
+	    // TODO Auto-generated method stub
+	    
+    }
 }
