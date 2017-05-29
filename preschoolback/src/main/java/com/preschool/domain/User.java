8856d83b4f5dfc3b671c098bcc3feed720a55f2c
Francis Phiri <franzparks@gmail.com>
2017-05-28 23:09:19 -0700

    implement the rest of methods

--- preschoolback/src/main/java/com/preschool/domain/User.java
+++ preschoolback/src/main/java/com/preschool/domain/User.java
@@ -145,20 +145,17 @@ private static final long serialVersionUID = 902783495L;
 
 	@Override
     public boolean isAccountNonExpired() {
-	    // TODO Auto-generated method stub
-	    return false;
+	    return true;
     }
 
 	@Override
     public boolean isAccountNonLocked() {
-	    // TODO Auto-generated method stub
-	    return false;
+	    return true;
     }
 
 	@Override
     public boolean isCredentialsNonExpired() {
-	    // TODO Auto-generated method stub
-	    return false;
+	    return true;
     }
 	
 	
