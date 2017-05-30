679d6b06606abe722bb42fb58f26a090f8eac59f
Francis Phiri <franzparks@gmail.com>
2017-05-29 17:09:41 -0700

    import dependencies

--- preschoolfront/src/app/components/my-profile/my-profile.component.ts
+++ preschoolfront/src/app/components/my-profile/my-profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AppConst } from '../../constants/app-const';
+import { UserService } from '../../services/user.service';
+import { LoginService } from '../../services/login.service';
+import { User } from '../../models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-profile',
