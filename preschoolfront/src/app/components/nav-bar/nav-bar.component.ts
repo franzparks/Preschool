d6fe9b9155fadd5fc10bed7c4822fe71e9eb4489
Francis Phiri <franzparks@gmail.com>
2017-05-30 13:16:06 -0700

    import dependencies

--- preschoolfront/src/app/components/nav-bar/nav-bar.component.ts
+++ preschoolfront/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
 import { Router, NavigationExtras } from '@angular/router';
+import {SchoolService} from '../../services/school.service';
 
 @Component({
   selector: 'app-nav-bar',
