236c9e56afdd8a69649dffa555548aad3520300b
Francis Phiri <franzparks@gmail.com>
2017-06-01 14:28:19 -0700

    import RatingAndReview

--- preschoolfront/src/app/services/add-rating.service.ts
+++ preschoolfront/src/app/services/add-rating.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
 
+import {RatingAndReview} from '../models/rating-and-review';
+
 @Injectable()
 export class AddRatingService {
 
