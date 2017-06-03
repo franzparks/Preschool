b31018821b373218849fa7b2db236f737cf97667
Francis Phiri <franzparks@gmail.com>
2017-06-03 02:45:51 -0700

    add sendSchool

--- preschoolfront/src/app/services/school.service.ts
+++ preschoolfront/src/app/services/school.service.ts
@@ -28,4 +28,15 @@ export class SchoolService {
   	return this.http.get(url, {headers: tokenHeader});
   }
 
+  sendSchool(school:School) {
+    let url = "http://localhost:8080/school/add";
+    
+    let headers = new Headers ({
+      'Content-Type': 'application/json',
+      'x-auth-token' : localStorage.getItem('xAuthToken')
+    });
+
+    return this.http.post(url, JSON.stringify(review), {headers: headers});
+  }
+
 }
