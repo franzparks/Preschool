ecd987746265534ef12d01634c75c685847f4a1f
Francis Phiri <franzparks@gmail.com>
2017-06-02 23:44:24 -0700

    import AppConst

--- preschoolfront/src/app/services/rating-and-review.service.ts
+++ preschoolfront/src/app/services/rating-and-review.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
+import {AppConst} from '../constants/app-consts';
+
 @Injectable()
 export class RatingAndReviewService {
 
