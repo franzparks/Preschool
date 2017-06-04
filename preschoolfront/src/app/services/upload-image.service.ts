4c4950d2747bae0219833eaef7ba46ba5edf4cb6
Francis Phiri <franzparks@gmail.com>
2017-06-04 00:24:35 -0700

    add modify method

--- preschoolfront/src/app/services/upload-image.service.ts
+++ preschoolfront/src/app/services/upload-image.service.ts
@@ -11,11 +11,22 @@ export class UploadImageService {
   	}
 
   	upload(schoolId: number) {
-  	this.makeFileRequest("http://localhost:8080/school/add/image?id="+schoolId, [], this.filesToUpload).then((result) => {
-  		console.log(result);
-  	}, (error) => {
-  		console.log(error);
-  	});
-  }
+  		this.makeFileRequest("http://localhost:8080/school/add/image?id="+schoolId, [], this.filesToUpload).then((result) => {
+  			console.log(result);
+  		}, (error) => {
+  			console.log(error);
+  		});
+  	}
+
+  	modify(schoolId: number) {
+    	console.log(this.filesToUpload);
+    	if (this.filesToUpload.length >0 ) {
+      		this.makeFileRequest("http://localhost:8080/school/update/image?id="+schoolId, [], this.filesToUpload).then((result) => {
+      			console.log(result);
+    		}, (error) => {
+      			console.log(error);
+    		});
+    	}
+  	}
 
 }
