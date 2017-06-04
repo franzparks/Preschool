0029331e330a689a1f64a03a3cd3241efa88dea5
Francis Phiri <franzparks@gmail.com>
2017-06-04 00:27:39 -0700

    add makeFileRequest method

--- preschoolfront/src/app/services/upload-image.service.ts
+++ preschoolfront/src/app/services/upload-image.service.ts
@@ -33,4 +33,27 @@ export class UploadImageService {
   		this.filesToUpload = <Array<File>> fileInput.target.files;
   	}
 
+  	makeFileRequest(url:string, params:Array<string>, files:Array<File>) {
+	  	return new Promise((resolve, reject) => {
+	  		var formData:any = new FormData();
+	  		var xhr = new XMLHttpRequest();
+	  		for(var i=0; i < files.length;i++) {
+	  			formData.append("uploads[]", files[i], files[i].name);
+	  		}
+	  		xhr.onreadystatechange = function() {
+	  			if(xhr.readyState == 4) {
+	  				if(xhr.status==200) {
+	  					console.log("image uploaded successfully!");
+	  				} else {
+	  					reject(xhr.response);
+	  				}
+	  			}
+	  		}
+
+	  		xhr.open("POST", url, true);
+	  		xhr.setRequestHeader("x-auth-token", localStorage.getItem("xAuthToken"));
+	  		xhr.send(formData);
+	  	});
+	}
+
 }
