fadfd921fad478b642b38e60a21d9a6609d1f9c0
Francis Phiri <franzparks@gmail.com>
2017-07-17 11:40:03 -0700

    user server path

--- preschoolfront/src/app/services/add-rating.service.ts
+++ preschoolfront/src/app/services/add-rating.service.ts
@@ -6,17 +6,19 @@ import {RatingAndReview} from '../models/rating-and-review';
 @Injectable()
 export class AddRatingService {
 
-  constructor(private http:Http) { }
+	private serverPath:string = AppConst.serverPath;
 
-  sendReview(review:RatingAndReview) {
-  	let url = "http://localhost:8080/reviews/add";
-    
-    let headers = new Headers ({
-      'Content-Type': 'application/json',
-      'x-auth-token' : localStorage.getItem('xAuthToken')
-    });
+  	constructor(private http:Http) { }
 
-    return this.http.post(url, JSON.stringify(review), {headers: headers});
-  }
+  	sendReview(review:RatingAndReview) {
+	  	let url = this.serverPath + "/reviews/add";
+	    
+	    let headers = new Headers ({
+	      'Content-Type': 'application/json',
+	      'x-auth-token' : localStorage.getItem('xAuthToken')
+	    });
+
+	    return this.http.post(url, JSON.stringify(review), {headers: headers});
+  	}
 
 }
