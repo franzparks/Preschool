15a35f5dc050a7df5a78d3036ae14a0d5bff5bf0
Francis Phiri <franzparks@gmail.com>
2017-07-17 12:25:00 -0700

    refactor: change ServerUrl to serverPath

--- preschoolfront/src/app/services/school.service.ts
+++ preschoolfront/src/app/services/school.service.ts
@@ -9,18 +9,18 @@ import {AppConst} from '../constants/app-consts';
 @Injectable()
 export class SchoolService {
 
-  private serverUrl = AppConst.serverPath;
+  private serverPath = AppConst.serverPath;
 
 
 	constructor(private http: Http) { }
 
 	getSchoolList() {
-  	return this.http.get(this.serverUrl+ '/school/schoolList', { withCredentials: true });
+  	return this.http.get(this.serverPath+ '/school/schoolList', { withCredentials: true });
 	}
 
 	getSchool(id: number) {
 
-  	let url = this.serverUrl+'/school/'+id;
+  	let url = this.serverPath+'/school/'+id;
 
   	let tokenHeader = new Headers({
   		'Content-Type' : 'application/json',
@@ -31,7 +31,7 @@ export class SchoolService {
 
   sendSchool(school:School) {
 
-    let url = this.serverUrl+ '/school/add';
+    let url = this.serverPath+ '/school/add';
     
     let headers = new Headers ({
       'Content-Type': 'application/json',
