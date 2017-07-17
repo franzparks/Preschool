1ba691fcad0410d7a25bca9ba21498d62c01865f
Francis Phiri <franzparks@gmail.com>
2017-07-17 11:38:19 -0700

    import consts

--- preschoolfront/src/app/services/add-rating.service.ts
+++ preschoolfront/src/app/services/add-rating.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
-
+import {AppConst} from '../constants/app-consts';
 import {RatingAndReview} from '../models/rating-and-review';
 
 @Injectable()
