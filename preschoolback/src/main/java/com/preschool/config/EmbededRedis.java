45e0e5fefcd8278a69db2d09603f7e32b3d1acbc
Francis Phiri <franzparks@gmail.com>
2017-07-26 21:07:00 -0700

    update redis config class

--- preschoolback/src/main/java/com/preschool/config/EmbededRedis.java
+++ preschoolback/src/main/java/com/preschool/config/EmbededRedis.java
@@ -3,10 +3,36 @@
  */
 package com.preschool.config;
 
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import redis.embedded.RedisServer;
+
 /**
  * @author francisphiri
  *
  */
+@Component
 public class EmbededRedis {
 	
+	@Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() throws IOException {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        redisServer.stop();
+    }
 }
