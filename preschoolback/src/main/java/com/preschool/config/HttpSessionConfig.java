7b166bc7f9b978e53434218036000560831d4b56
Francis Phiri <franzparks@gmail.com>
2017-07-26 22:48:38 -0700

    update configs

--- preschoolback/src/main/java/com/preschool/config/HttpSessionConfig.java
+++ preschoolback/src/main/java/com/preschool/config/HttpSessionConfig.java
@@ -15,7 +15,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 public class HttpSessionConfig {
 	@Bean
 	public LettuceConnectionFactory connectionFactory() {
-		return new LettuceConnectionFactory("http://eyas-redis.ajkq9r.ng.0001.usw1.cache.amazonaws.com",6379);
-		//return new LettuceConnectionFactory();
+		//return new LettuceConnectionFactory("http://eyas-redis.ajkq9r.ng.0001.usw1.cache.amazonaws.com",6379);
+		return new LettuceConnectionFactory();
 	}
 }
