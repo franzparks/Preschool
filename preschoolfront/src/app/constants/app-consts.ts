fed80c7ac037966ab5f168980be2422979aaf442
Francis Phiri <franzparks@gmail.com>
2017-09-05 06:10:37 -0700

    update allowed origin

--- preschoolfront/src/app/constants/app-consts.ts
+++ preschoolfront/src/app/constants/app-consts.ts
@@ -1,4 +1,4 @@
 export class AppConst {
-	public static serverPath = 'http://spingbooteb1-env.us-west-1.elasticbeanstalk.com/'; //'http://127.0.0.1:8080';
+	public static serverPath = 'http://spingbooteb1-env.us-west-1.elasticbeanstalk.com'; //'http://127.0.0.1:8080';
 	//public static serverPath = 'http://127.0.0.1:5000';
 }
