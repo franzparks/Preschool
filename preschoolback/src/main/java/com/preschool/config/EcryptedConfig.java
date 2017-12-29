88c92270dc2a00c9916059b6b2f641c0e72c6a55
Francis Phiri <franzparks@gmail.com>
2017-12-29 13:08:02 -0800

    Add encryption class

---
+++ preschoolback/src/main/java/com/preschool/config/EcryptedConfig.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.preschool.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author francisphiri
+ *
+ */
+@Configuration
+public class EcryptedConfig {
+	
+}
