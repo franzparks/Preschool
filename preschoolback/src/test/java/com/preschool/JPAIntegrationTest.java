7e6aa6268bb9c8412d8a39563dbb966a734cf426
Francis Phiri <franzparks@gmail.com>
2018-01-02 22:44:55 -0800

    Add integration test info

--- preschoolback/src/test/java/com/preschool/JPAIntegrationTest.java
+++ preschoolback/src/test/java/com/preschool/JPAIntegrationTest.java
@@ -3,10 +3,30 @@
  */
 package com.preschool;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.preschool.config.repository.SchoolRepository;
+
 /**
  * @author francisphiri
  *
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PreschoolApplication.class)
 public class JPAIntegrationTest {
 	
+	@Autowired
+	private SchoolRepository schoolRepository;
+	
+	@Test
+    public void givenSchoolRepository_whenSaveAndRetreiveSchool_thenOK() {
+		assertTrue(true);
+		//School newSchool = schoolRepository.save(new School());
+	}
 }
