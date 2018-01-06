540022f4e4ef01901edbeee29b3bd48556a7ab2e
Francis Phiri <franzparks@gmail.com>
2018-01-06 11:31:07 -0800

    Update security configs and add mmore props

--- preschoolback/src/main/java/com/preschool/config/RequestFilter.java
+++ preschoolback/src/main/java/com/preschool/config/RequestFilter.java
@@ -31,8 +31,8 @@ public class RequestFilter implements Filter{
         HttpServletRequest request = (HttpServletRequest) req;
        
         //response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-    
         response.setHeader("Access-Control-Allow-Origin", "http://eyaseyes.com");
+        
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
         response.setHeader("Access-Control-Max-Age", "3600");
