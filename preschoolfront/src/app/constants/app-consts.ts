30dccbe8d40675a75ca266b52796eac12858d3d5
Francis Phiri <franzparks@gmail.com>
2018-03-23 16:32:04 -0700

    Update redis config

--- preschoolfront/src/app/constants/app-consts.ts
+++ preschoolfront/src/app/constants/app-consts.ts
@@ -1,4 +1,4 @@
 export class AppConst {
-	//public static serverPath = 'http://backend.eyaseyes.com/';
-	public static serverPath = 'http://127.0.0.1:8080';
+	public static serverPath = 'http://backend.eyaseyes.com/';
+	//public static serverPath = 'http://127.0.0.1:8080';
 }
