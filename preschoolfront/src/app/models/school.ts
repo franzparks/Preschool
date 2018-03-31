ffabbda97db4368feb83e6600b415eeee118428e
Francis Phiri <franzparks@gmail.com>
2018-03-31 14:06:50 -0700

    Add model and form fields

--- preschoolfront/src/app/models/school.ts
+++ preschoolfront/src/app/models/school.ts
@@ -2,15 +2,17 @@ import {RatingAndReview} from './rating-and-review';
 export class School {
 	id: number;
 	ratingAndReviewsList: RatingAndReview;
+	name:string;
+	type:string;
+	category:string;
 	teacherToStudentRatio: string;
-	title: string;
-	summary: string;
 	ageRange: string;
 	priceRange: string;
 	schedule: string;
 	address: string;
 	phone: string;
 	website: string;
+	summary: string;
 	averageRating: number;
 	schoolImage: string;
 }
